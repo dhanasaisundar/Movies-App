c82b232acd60e472dbce5c8a647ea67e9384bf18
dhanasaisundar <dhanasaisundar1997@gmail.com>
2023-10-27 12:48:52 +0000

    Login Route Completion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+import LogInForm from './components/LogInForm'
 import './App.css'
 
-const App = () => <div>Hello World</div>
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={LogInForm} />
+      </Switch>
+    )
+  }
+}
 
 export default App
